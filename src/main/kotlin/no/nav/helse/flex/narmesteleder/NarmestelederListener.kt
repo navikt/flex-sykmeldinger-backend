009ae85901e5ee0c709d5cdb2b66a7adf720daad
golshidbahadorian <golshid.bahadorian@nav.no>
2024-10-15 13:55:55 +0200

    Henter og lagrer nærmesteleder

---
+++ src/main/kotlin/no/nav/helse/flex/narmesteleder/NarmestelederListener.kt
@@ -0,0 +1,25 @@
+package no.nav.helse.flex.narmesteleder
+
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.stereotype.Component
+
+@Component
+class NarmestelederListener(
+    private val oppdateringAvNarmesteLeder: OppdateringAvNarmesteLeder,
+) {
+    @KafkaListener(
+        topics = [NARMESTELEDER_LEESAH_TOPIC],
+        containerFactory = "aivenKafkaListenerContainerFactory",
+    )
+    fun listen(
+        cr: ConsumerRecord<String, String>,
+        acknowledgment: Acknowledgment,
+    ) {
+        oppdateringAvNarmesteLeder.behandleMeldingFraKafka(cr.value())
+        acknowledgment.acknowledge()
+    }
+}
+
+const val NARMESTELEDER_LEESAH_TOPIC = "teamsykmelding.syfo-narmesteleder-leesah"
