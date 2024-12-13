60fd8b1684b49f871cc8bb4f6a31387e573aecab
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-12-13 16:28:37 +0100

    setter RegistrertePersonerForArbeidsforhold til å returnere fra env var

--- src/test/kotlin/no/nav/helse/flex/arbeidsforhold/innhenting/aareghendelser/AaregHendelserConsumerTest.kt
+++ src/test/kotlin/no/nav/helse/flex/arbeidsforhold/innhenting/aareghendelser/AaregHendelserConsumerTest.kt
@@ -13,7 +13,6 @@ import no.nav.helse.flex.objectMapper
 import org.amshove.kluent.invoking
 import org.amshove.kluent.`should be`
 import org.amshove.kluent.`should be equal to`
-import org.amshove.kluent.`should not be`
 import org.amshove.kluent.`should throw`
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.consumer.ConsumerRecords
