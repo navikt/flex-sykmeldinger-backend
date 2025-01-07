d00538987d2e598dc3ec643ad2fd1207743b8806
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-01-07 16:23:37 +0100

    add repository stuff

---
+++ src/main/kotlin/no/nav/helse/flex/sykmelding/repository/SykmeldingRepository.kt
@@ -0,0 +1,45 @@
+package no.nav.helse.flex.sykmelding.repository
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import java.time.Instant
+
+
+@Repository
+interface SykmeldingRepository : CrudRepository<SykmeldingDbRecord, String> {
+    fun findBySykmeldingId(sykmeldingId: String): SykmeldingDbRecord?
+    fun findByFnr(fnr: String): List<SykmeldingDbRecord>
+}
+
+@Repository
+interface SykmeldingStatusRepository : CrudRepository<SykmeldingStatusDbRecord, String> {
+    fun findBySykmeldingId(sykmeldingId: String): List<SykmeldingStatusDbRecord>
+    fun findFirstBySykmeldingIdOrderByTimestampDesc(sykmeldingId: String): SykmeldingStatusDbRecord?
+}
+
+data class SykmeldingDbRecord(
+    @Id
+    val id: String? = null,
+    val sykmeldingId: String,
+    val fnr: String,
+    val behandlingsutfall: String, // JSONB
+    val sykmelding: String, // JSONB
+    val latestStatusId: String?,
+    val opprettet: Instant,
+    val sendt: Instant? = null,
+    val bekreftet: Instant? = null,
+    val utgatt: Instant? = null,
+    val avbrutt: Instant? = null,
+)
+
+data class SykmeldingStatusDbRecord(
+    @Id
+    val id: String? = null,
+    val sykmeldingId: String,
+    val timestamp: Instant,
+    val status: String,
+    val arbeidsgiver: String?, // JSONB
+    val sporsmal: String?, // JSONB
+    val opprettet: Instant,
+)
