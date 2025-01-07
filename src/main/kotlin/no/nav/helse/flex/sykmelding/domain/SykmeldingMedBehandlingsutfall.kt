5cbe452300239f3e0bc7fb677a010ac9033c3bfc
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-01-07 14:19:11 +0100

    Tidlig arbeide sykmelding mock

--- src/main/kotlin/no/nav/helse/flex/sykmelding/domain/SykmeldingMedBehandlingsutfall.kt
+++ src/main/kotlin/no/nav/helse/flex/sykmelding/domain/SykmeldingMedBehandlingsutfall.kt
@@ -1,3 +1,19 @@
 package no.nav.helse.flex.sykmelding.domain
+import java.time.LocalDate
 
-class SykmeldingMedBehandlingsutfall
+data class SykmeldingMedBehandlingsutfall(
+    val sykmelding: Sykmelding,
+    val behandlingsutfall: Behandlingsutfall,
+)
+
+// OK, MANUAL_PROCESSING, INVALID
+data class Behandlingsutfall(
+    val status: String,
+)
+
+data class TidligereArbeidsgiverDTO(
+    val orgnummer: String,
+    val orgNavn: String,
+    val fom: LocalDate,
+    val tom: LocalDate,
+)
