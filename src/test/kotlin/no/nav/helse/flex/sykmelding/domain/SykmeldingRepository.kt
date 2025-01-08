aa9eddad81843d6764a6066607bb7d987b0e663b
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-01-08 16:48:34 +0100

    Legger inn repositories

---
+++ src/test/kotlin/no/nav/helse/flex/sykmelding/domain/SykmeldingRepository.kt
@@ -0,0 +1,24 @@
+package no.nav.helse.flex.sykmelding.domain
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import java.time.Instant
+
+@Repository
+interface SykmeldingRepository : CrudRepository<SykmeldingDbRecord, String> {
+    fun findByFnr(fnr: String): List<SykmeldingDbRecord>
+    fun findBySykmeldingUuid(sykmeldingUuid: String): SykmeldingDbRecord?
+}
+
+data class SykmeldingDbRecord(
+    @Id
+    val id: String? = null,
+    val sykmeldingUuid: String,
+    val sisteSykmeldingstatusId: String,
+    val fnr: String,
+    val sykmelding: String,
+    val person: String,
+    val opprettet: Instant,
+    val oppdatert: Instant?
+)
