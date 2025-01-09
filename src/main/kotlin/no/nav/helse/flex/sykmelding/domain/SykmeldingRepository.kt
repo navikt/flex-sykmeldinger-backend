d6392a5e6126e193ff046f90a0cee69e5a74be5e
Eirik Skjærseth <eirik.skjaerseth@gmail.com>
2025-01-09 13:40:47 +0100

    leg til domene repository

--- src/main/kotlin/no/nav/helse/flex/sykmelding/domain/SykmeldingRepository.kt
+++ src/main/kotlin/no/nav/helse/flex/sykmelding/domain/SykmeldingRepository.kt
@@ -1,12 +1,67 @@
 package no.nav.helse.flex.sykmelding.domain
 
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.helse.flex.objectMapper
+import no.nav.helse.flex.serialisertTilString
+import org.postgresql.util.PGobject
 import org.springframework.data.annotation.Id
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.time.Instant
 
+interface ISykmeldingRepository {
+    fun save(sykmelding: ISykmelding)
+
+    fun findBySykmeldingUuid(id: String): ISykmelding?
+
+    fun findByFnr(fnr: String): List<ISykmelding>
+}
+
 @Repository
-interface SykmeldingRepository : CrudRepository<SykmeldingDbRecord, String> {
+class SykmeldingRepository(
+    private val sykmeldingDbRepository: SykmeldingDbRepository,
+) : ISykmeldingRepository {
+    override fun save(sykmelding: ISykmelding) {
+        val dbRecord =
+            SykmeldingDbRecord(
+                sykmeldingUuid = sykmelding.id,
+                sisteSykmeldingstatusId = "",
+                fnr = sykmelding.pasient.fnr,
+                sykmelding =
+                    PGobject().apply {
+                        type = "json"
+                        value = sykmelding.serialisertTilString()
+                    },
+                opprettet = Instant.now(),
+                oppdatert = Instant.now(),
+            )
+        sykmeldingDbRepository.save(dbRecord)
+    }
+
+    override fun findBySykmeldingUuid(id: String): ISykmelding? {
+        val dbRecord = sykmeldingDbRepository.findBySykmeldingUuid(id)
+        if (dbRecord == null) {
+            return null
+        }
+        return mapDbRecordTilSykmelding(dbRecord)
+    }
+
+    override fun findByFnr(fnr: String): List<ISykmelding> {
+        val dbRecords = sykmeldingDbRepository.findByFnr(fnr)
+        return dbRecords.map(this::mapDbRecordTilSykmelding)
+    }
+
+    private fun mapDbRecordTilSykmelding(dbRecord: SykmeldingDbRecord): ISykmelding {
+        val serialisertSykmelding = dbRecord.sykmelding
+        check(serialisertSykmelding.value != null) {
+            "sykmelding kolonne burde ikke være null"
+        }
+        return objectMapper.readValue(serialisertSykmelding.value!!)
+    }
+}
+
+@Repository
+interface SykmeldingDbRepository : CrudRepository<SykmeldingDbRecord, String> {
     fun findByFnr(fnr: String): List<SykmeldingDbRecord>
 
     fun findBySykmeldingUuid(sykmeldingUuid: String): SykmeldingDbRecord?
@@ -18,8 +73,7 @@ data class SykmeldingDbRecord(
     val sykmeldingUuid: String,
     val sisteSykmeldingstatusId: String,
     val fnr: String,
-    val sykmelding: String,
-    val person: String,
+    val sykmelding: PGobject,
     val opprettet: Instant,
     val oppdatert: Instant?,
 )
