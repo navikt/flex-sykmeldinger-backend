42c5b3205d85e526a83dc0d260959917f4861441
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-01-09 13:32:59 +0100

    Tilbakestiller listener etter lesing av nærmeste leder topic på nytt og skrur av settingen auto offset reset til earliest

--- src/main/kotlin/no/nav/helse/flex/narmesteleder/NarmestelederListener.kt
+++ src/main/kotlin/no/nav/helse/flex/narmesteleder/NarmestelederListener.kt
@@ -1,23 +1,18 @@
 package no.nav.helse.flex.narmesteleder
 
-import no.nav.helse.flex.logger
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.common.TopicPartition
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 
 @Component
 class NarmestelederListener(
     private val oppdateringAvNarmesteLeder: OppdateringAvNarmesteLeder,
-) : ConsumerSeekAware {
-    val log = logger()
+) {
 
     @KafkaListener(
         topics = [NARMESTELEDER_LEESAH_TOPIC],
         containerFactory = "aivenKafkaListenerContainerFactory",
-        properties = ["auto.offset.reset = earliest"],
     )
     fun listen(
         cr: ConsumerRecord<String, String>,
@@ -25,16 +20,6 @@ class NarmestelederListener(
     ) {
         oppdateringAvNarmesteLeder.behandleMeldingFraKafka(cr.value())
         acknowledgment.acknowledge()
-    }
-
-    override fun onPartitionsAssigned(
-        assignments: Map<TopicPartition, Long>,
-        callback: ConsumerSeekAware.ConsumerSeekCallback,
-    ) {
-        log.info("Partitions assigned, seeking to beginning")
-        assignments.keys.forEach { topicPartition ->
-            callback.seekToBeginning(topicPartition.topic(), topicPartition.partition())
-        }
     }
 }
 
