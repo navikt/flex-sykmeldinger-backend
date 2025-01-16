2a5a6c774486548a04260ce96aae4cf06e17477c
Eirik Skjærseth <eirik.skjaerseth@gmail.com>
2025-01-16 12:51:59 +0100

    refactor deduplisering test

--- src/test/kotlin/no/nav/helse/flex/sykmelding/SykmeldingLagrerIntegrasjonTest.kt
+++ src/test/kotlin/no/nav/helse/flex/sykmelding/SykmeldingLagrerIntegrasjonTest.kt
@@ -2,6 +2,7 @@ package no.nav.helse.flex.sykmelding
 
 import no.nav.helse.flex.FellesTestOppsett
 import no.nav.helse.flex.kafka.SYKMELDING_TOPIC
+import no.nav.helse.flex.kafka.SykmeldingListener
 import no.nav.helse.flex.serialisertTilString
 import no.nav.helse.flex.sykmelding.domain.SykmeldingMedBehandlingsutfallMelding
 import no.nav.helse.flex.sykmelding.domain.lagSykmeldingGrunnlag
@@ -9,17 +10,22 @@ import no.nav.helse.flex.sykmelding.domain.lagValidation
 import no.nav.helse.flex.testdata.TEST_SYKMELDING_TOPIC
 import org.amshove.kluent.`should be equal to`
 import org.amshove.kluent.shouldNotBeNull
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.awaitility.Awaitility.await
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.support.Acknowledgment
 import java.time.Duration
 import java.time.Instant
 import java.util.function.Supplier
+
+private val NOOP_ACK = Acknowledgment {}
 
 class SykmeldingLagrerIntegrasjonTest : FellesTestOppsett() {
     @Configuration
@@ -29,6 +35,9 @@ class SykmeldingLagrerIntegrasjonTest : FellesTestOppsett() {
             return Supplier { Instant.parse("2020-01-01T00:00:00.000Z") }
         }
     }
+
+    @Autowired
+    lateinit var sykmeldingListener: SykmeldingListener
 
     @BeforeAll
     fun setup() {
@@ -75,32 +84,19 @@ class SykmeldingLagrerIntegrasjonTest : FellesTestOppsett() {
                 validation = lagValidation(),
             )
 
-        repeat(2) {
-            kafkaProducer.send(
-                ProducerRecord(
+        repeat(2) { i ->
+            sykmeldingListener.listen(
+                ConsumerRecord(
                     topic,
-                    null,
+                    -1,
+                    i.toLong(),
                     kafkaMelding.sykmelding.id,
                     kafkaMelding.serialisertTilString(),
                 ),
-            ).get()
+                NOOP_ACK,
+            )
         }
-        // for å kontrollere at de forrige to ble lest før vi asserter
-        kafkaProducer.send(
-            ProducerRecord(
-                topic,
-                null,
-                kafkaMelding.sykmelding.id,
-                SykmeldingMedBehandlingsutfallMelding(
-                    sykmelding = lagSykmeldingGrunnlag(id = "2"),
-                    validation = lagValidation(),
-                ).serialisertTilString(),
-            ),
-        ).get()
 
-        await()
-            .atMost(Duration.ofSeconds(10)).untilAsserted {
-                sykemeldingRepository.findAll().size `should be equal to` 2
-            }
+        sykemeldingRepository.findAll().size `should be equal to` 1
     }
 }
