eb80fc86acfe974c9b3f3b57da2a3b2bc1577a9e
Eirik Skjærseth <eirik.skjaerseth@gmail.com>
2025-01-23 13:40:33 +0100

    set opp test oppsett for fakes

---
+++ src/test/kotlin/no/nav/helse/flex/FakesTestOppsett.kt
@@ -0,0 +1,120 @@
+package no.nav.helse.flex
+
+import no.nav.helse.flex.arbeidsforhold.ArbeidsforholdRepository
+import no.nav.helse.flex.narmesteleder.NARMESTELEDER_LEESAH_TOPIC
+import no.nav.helse.flex.narmesteleder.NarmesteLederRepository
+import no.nav.helse.flex.narmesteleder.domain.NarmesteLederLeesah
+import no.nav.helse.flex.sykmelding.SykmeldingRepositoryFake
+import no.nav.helse.flex.sykmelding.domain.ISykmeldingRepository
+import no.nav.security.mock.oauth2.MockOAuth2Server
+import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry
+import org.springframework.kafka.test.utils.ContainerTestUtils
+import org.springframework.test.web.servlet.MockMvc
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.kafka.KafkaContainer
+import org.testcontainers.utility.DockerImageName
+
+private class PostgreSQLContainer14V2 : PostgreSQLContainer<PostgreSQLContainer14V2>("postgres:14-alpine")
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@AutoConfigureObservability
+@EnableMockOAuth2Server
+@SpringBootTest(
+    classes = [Application::class, FakesTestOppsett.TestConfig::class],
+    properties = ["spring.main.allow-bean-definition-overriding=true"],
+)
+@AutoConfigureMockMvc(print = MockMvcPrint.NONE, printOnlyOnFailure = false)
+abstract class FakesTestOppsett {
+    @TestConfiguration
+    class TestConfig {
+        @Bean
+        fun sykmeldingRepository(): ISykmeldingRepository = SykmeldingRepositoryFake()
+    }
+
+    @Autowired
+    lateinit var kafkaConsumer: KafkaConsumer<String, String>
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Autowired
+    lateinit var server: MockOAuth2Server
+
+    @Autowired
+    lateinit var kafkaProducer: Producer<String, String>
+
+    @Autowired
+    lateinit var narmesteLederRepository: NarmesteLederRepository
+
+    @Autowired
+    lateinit var arbeidsforholdRepository: ArbeidsforholdRepository
+
+    @Autowired
+    lateinit var sykmeldingRepository: ISykmeldingRepository
+
+    @Autowired
+    lateinit var kafkaListenerRegistry: KafkaListenerEndpointRegistry
+
+    companion object {
+        init {
+
+            KafkaContainer(DockerImageName.parse("apache/kafka-native:3.8.1")).apply {
+                start()
+                System.setProperty("KAFKA_BROKERS", bootstrapServers)
+            }
+
+            PostgreSQLContainer14V2().apply {
+                withCommand("postgres", "-c", "wal_level=logical")
+                start()
+                System.setProperty("spring.datasource.url", "$jdbcUrl&reWriteBatchedInserts=true")
+                System.setProperty("spring.datasource.username", username)
+                System.setProperty("spring.datasource.password", password)
+            }
+
+            startMockWebServere()
+        }
+    }
+
+    @AfterAll
+    fun `Vi resetter databasen`() {
+        slettDatabase()
+    }
+
+    fun slettDatabase() {
+        narmesteLederRepository.deleteAll()
+        arbeidsforholdRepository.deleteAll()
+        sykmeldingRepository.deleteAll()
+    }
+
+    fun ventPaConsumers() {
+        // Burde brukes dersom consumere har offset=latest
+        kafkaListenerRegistry.listenerContainers.forEach { container ->
+            ContainerTestUtils.waitForAssignment(container, 1)
+        }
+    }
+
+    fun sendNarmesteLederLeesah(nl: NarmesteLederLeesah) {
+        kafkaProducer
+            .send(
+                ProducerRecord(
+                    NARMESTELEDER_LEESAH_TOPIC,
+                    null,
+                    nl.narmesteLederId.toString(),
+                    nl.serialisertTilString(),
+                ),
+            ).get()
+    }
+}
---
+++ src/test/kotlin/no/nav/helse/flex/sykmelding/FakeRepositoryTest.kt
@@ -0,0 +1,35 @@
+package no.nav.helse.flex.sykmelding
+
+import no.nav.helse.flex.FakesTestOppsett
+import no.nav.helse.flex.sykmelding.domain.SykmeldingMedBehandlingsutfallMelding
+import no.nav.helse.flex.sykmelding.domain.lagSykmeldingGrunnlag
+import no.nav.helse.flex.sykmelding.domain.lagValidation
+import no.nav.helse.flex.sykmelding.logikk.SykmeldingLagrer
+import org.amshove.kluent.shouldBe
+import org.amshove.kluent.shouldBeInstanceOf
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+class FakeRepositoryTest : FakesTestOppsett() {
+    @Autowired
+    private lateinit var sykmeldingLagrer: SykmeldingLagrer
+
+    @Test
+    fun `burde injecte fake repository`() {
+        sykmeldingRepository.shouldBeInstanceOf(SykmeldingRepositoryFake::class)
+    }
+
+    @Test
+    fun `sykmelding lagrer bruker fake repo`() {
+        val kafkaMelding =
+            SykmeldingMedBehandlingsutfallMelding(
+                sykmelding = lagSykmeldingGrunnlag(id = "1"),
+                validation = lagValidation(),
+            )
+
+        sykmeldingLagrer.lagreSykmeldingMedBehandlingsutfall(kafkaMelding)
+
+        sykmeldingRepository.shouldBeInstanceOf(SykmeldingRepositoryFake::class)
+        sykmeldingRepository.findAll().size shouldBe 1
+    }
+}
