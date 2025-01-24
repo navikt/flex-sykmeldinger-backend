182c500f5be051ff85326a88e6acdba4f9bf71b0
Eirik Skjærseth <eirik.skjaerseth@gmail.com>
2025-01-24 17:06:16 +0100

    bruk abstrakt CrudRepoFake i NarmesteLederRepositoryFake

--- src/test/kotlin/no/nav/helse/flex/narmesteleder/NarmesteLederRepositoryFake.kt
+++ src/test/kotlin/no/nav/helse/flex/narmesteleder/NarmesteLederRepositoryFake.kt
@@ -1,65 +1,17 @@
 package no.nav.helse.flex.narmesteleder
 
+import no.nav.helse.flex.AbstractCrudRepositoryFake
 import no.nav.helse.flex.narmesteleder.domain.NarmesteLeder
 import java.util.*
 
-class NarmesteLederRepositoryFake : NarmesteLederRepository {
-    val lagretNarmesteLeder: MutableMap<String, NarmesteLeder> = mutableMapOf()
-
-    private fun lagId(): String = UUID.randomUUID().toString()
-
-    override fun <S : NarmesteLeder?> save(entity: S & Any): S & Any {
-        val entityWithId =
-            if (entity.id == null) {
-                entity.copy(id = lagId())
-            } else {
-                entity
-            }
-        lagretNarmesteLeder[entityWithId.id!!] = entityWithId
-        @Suppress("UNCHECKED_CAST")
-        return entityWithId as (S & Any)
-    }
-
-    override fun <S : NarmesteLeder> saveAll(entities: MutableIterable<S>): MutableIterable<S> {
-        entities.forEach {
-            save(it)
-        }
-        return entities
-    }
-
+class NarmesteLederRepositoryFake :
+    AbstractCrudRepositoryFake<NarmesteLeder>(
+        getEntityId = { it.id },
+        setEntityId = { entity, id -> entity.copy(id = id) },
+    ),
+    NarmesteLederRepository {
     override fun findByNarmesteLederId(narmesteLederId: UUID): NarmesteLeder? =
-        lagretNarmesteLeder.values.find { it.narmesteLederId == narmesteLederId }
+        entities.values.find { it.narmesteLederId == narmesteLederId }
 
-    override fun findAllByBrukerFnr(fnr: String): List<NarmesteLeder> = lagretNarmesteLeder.values.filter { it.brukerFnr == fnr }
-
-    override fun findById(id: String): Optional<NarmesteLeder> = Optional.ofNullable(lagretNarmesteLeder.values.find { it.id == id })
-
-    override fun existsById(id: String): Boolean = lagretNarmesteLeder.values.any { it.id == id }
-
-    override fun findAll(): MutableIterable<NarmesteLeder> = lagretNarmesteLeder.values
-
-    override fun findAllById(ids: MutableIterable<String>): MutableIterable<NarmesteLeder> =
-        lagretNarmesteLeder.values.filter { it.id in ids }.toMutableList()
-
-    override fun count(): Long = lagretNarmesteLeder.size.toLong()
-
-    override fun deleteById(id: String) {
-        lagretNarmesteLeder.remove(id)
-    }
-
-    override fun delete(entity: NarmesteLeder) {
-        lagretNarmesteLeder.remove(entity.id)
-    }
-
-    override fun deleteAllById(ids: MutableIterable<String>) {
-        ids.forEach { deleteById(it) }
-    }
-
-    override fun deleteAll(entities: MutableIterable<NarmesteLeder>) {
-        entities.forEach { delete(it) }
-    }
-
-    override fun deleteAll() {
-        lagretNarmesteLeder.clear()
-    }
+    override fun findAllByBrukerFnr(fnr: String): List<NarmesteLeder> = entities.values.filter { it.brukerFnr == fnr }
 }
