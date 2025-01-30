eb162c1ce0e0977c0ef73e06fad9280d448cd677
Eirik Skjærseth <eirik.skjaerseth@gmail.com>
2025-01-30 15:38:13 +0100

    hent arbeidsforhold i SykmeldingLagrer (fra kafka)

--- src/main/kotlin/no/nav/helse/flex/sykmelding/logikk/SykmeldingLagrer.kt
+++ src/main/kotlin/no/nav/helse/flex/sykmelding/logikk/SykmeldingLagrer.kt
@@ -1,23 +1,29 @@
 package no.nav.helse.flex.sykmelding.logikk
 
+import no.nav.helse.flex.arbeidsforhold.innhenting.ArbeidsforholdInnhentingService
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.sykmelding.domain.*
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 import java.time.Instant
 import java.util.function.Supplier
 
 @Component
 class SykmeldingLagrer(
     private val sykmeldingRepository: ISykmeldingRepository,
+    private val arbeidsforholdInnhentingService: ArbeidsforholdInnhentingService,
     private val nowFactory: Supplier<Instant>,
 ) {
     val log = logger()
 
+    @Transactional
     fun lagreSykmeldingMedBehandlingsutfall(sykmeldingMedBehandlingsutfall: SykmeldingMedBehandlingsutfallMelding) {
         if (sykmeldingRepository.findBySykmeldingId(sykmeldingMedBehandlingsutfall.sykmelding.id) != null) {
             log.info("Sykmelding ${sykmeldingMedBehandlingsutfall.sykmelding.id} finnes fra før")
         } else {
-            sykmeldingRepository.save(sykmeldingFactory(sykmeldingMedBehandlingsutfall))
+            val sykmelding = sykmeldingFactory(sykmeldingMedBehandlingsutfall)
+            sykmeldingRepository.save(sykmelding)
+            arbeidsforholdInnhentingService.synkroniserArbeidsforholdForPerson(sykmelding.pasientFnr)
             log.info("Sykmelding ${sykmeldingMedBehandlingsutfall.sykmelding.id} lagret")
         }
     }
--- src/test/kotlin/no/nav/helse/flex/sykmelding/SykmeldingLagrerFakeTest.kt
+++ src/test/kotlin/no/nav/helse/flex/sykmelding/SykmeldingLagrerFakeTest.kt
@@ -1,14 +1,24 @@
 package no.nav.helse.flex.sykmelding
 
 import no.nav.helse.flex.FakesTestOppsett
+import no.nav.helse.flex.arbeidsforhold.innhenting.EKSEMPEL_RESPONSE_FRA_EREG
+import no.nav.helse.flex.arbeidsforhold.innhenting.lagArbeidsforholdOversikt
+import no.nav.helse.flex.arbeidsforhold.innhenting.lagArbeidsforholdOversiktResponse
+import no.nav.helse.flex.notFoundDispatcher
+import no.nav.helse.flex.serialisertTilString
+import no.nav.helse.flex.simpleDispatcher
 import no.nav.helse.flex.sykmelding.domain.StatusEvent
 import no.nav.helse.flex.sykmelding.domain.SykmeldingMedBehandlingsutfallMelding
+import no.nav.helse.flex.sykmelding.domain.lagPasient
 import no.nav.helse.flex.sykmelding.domain.lagSykmeldingGrunnlag
 import no.nav.helse.flex.sykmelding.domain.lagValidation
 import no.nav.helse.flex.sykmelding.logikk.SykmeldingLagrer
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.amshove.kluent.`should be equal to`
 import org.amshove.kluent.shouldNotBeNull
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -16,9 +26,33 @@ class SykmeldingLagrerFakeTest : FakesTestOppsett() {
     @Autowired
     private lateinit var sykmeldingLagrer: SykmeldingLagrer
 
+    @Autowired
+    private lateinit var aaregMockWebServer: MockWebServer
+
+    @Autowired
+    private lateinit var eregMockWebServer: MockWebServer
+
+    @BeforeEach
+    fun setup() {
+        aaregMockWebServer.dispatcher =
+            simpleDispatcher {
+                lagJsonResponse(
+                    lagArbeidsforholdOversiktResponse(emptyList()).serialisertTilString(),
+                )
+            }
+
+        eregMockWebServer.dispatcher =
+            simpleDispatcher {
+                lagJsonResponse(EKSEMPEL_RESPONSE_FRA_EREG.serialisertTilString())
+            }
+    }
+
     @AfterEach
     fun tearDown() {
         slettDatabase()
+
+        aaregMockWebServer.dispatcher = notFoundDispatcher
+        eregMockWebServer.dispatcher = notFoundDispatcher
     }
 
     @Test
@@ -62,4 +96,35 @@ class SykmeldingLagrerFakeTest : FakesTestOppsett() {
         val status = sykmelding.statuser[0]
         status.status `should be equal to` StatusEvent.APEN
     }
+
+    @Test
+    fun `burde hente arbeidsforhold nar sykmelding lagres`() {
+        aaregMockWebServer.dispatcher =
+            simpleDispatcher {
+                lagJsonResponse(
+                    lagArbeidsforholdOversiktResponse(listOf(lagArbeidsforholdOversikt(fnr = "fnr"))).serialisertTilString(),
+                )
+            }
+
+        eregMockWebServer.dispatcher =
+            simpleDispatcher {
+                lagJsonResponse(EKSEMPEL_RESPONSE_FRA_EREG.serialisertTilString())
+            }
+
+        val sykmeldingMedBehandlingsutfall =
+            SykmeldingMedBehandlingsutfallMelding(
+                sykmelding = lagSykmeldingGrunnlag(id = "1", pasient = lagPasient(fnr = "fnr")),
+                validation = lagValidation(),
+            )
+
+        sykmeldingLagrer.lagreSykmeldingMedBehandlingsutfall(sykmeldingMedBehandlingsutfall)
+
+        val arbeidsforhold = arbeidsforholdRepository.getAllByFnr("fnr")
+        arbeidsforhold.size `should be equal to` 1
+    }
 }
+
+private fun lagJsonResponse(body: String) =
+    MockResponse()
+        .setBody(body)
+        .setHeader("Content-Type", "application/json")
