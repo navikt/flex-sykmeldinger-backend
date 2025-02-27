27a072cad5620e5f0bea497f4bebc5841fa66a22
Eirik HS <eirik.skjaerseth@gmail.com>
2025-02-27 11:04:14 +0100

    Bump org.apache.commons:commons-text from 1.12.0 to 1.13.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ val logstashLogbackEncoderVersion = "8.0"
 val kluentVersion = "1.73"
 val inntektsmeldingKontraktVersion = "2024.05.21-09-56-5528e"
 val sykepengesoknadKafkaVersion = "2024.12.06-09.11-55fbeea5"
-val commonsTextVersion = "1.12.0"
+val commonsTextVersion = "1.13.0"
 val mockitoKotlinVersion = "2.2.0"
 
 dependencies {
