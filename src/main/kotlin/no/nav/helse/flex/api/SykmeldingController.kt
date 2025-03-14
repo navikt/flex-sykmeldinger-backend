14ad8e560273ff41486eff0b4d0a8ecc9659e852
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-03-14 15:44:41 +0100

    wip

--- src/main/kotlin/no/nav/helse/flex/api/SykmeldingController.kt
+++ src/main/kotlin/no/nav/helse/flex/api/SykmeldingController.kt
@@ -9,6 +9,7 @@ import no.nav.helse.flex.config.TOKENX
 import no.nav.helse.flex.config.TokenxValidering
 import no.nav.helse.flex.narmesteleder.domain.NarmesteLeder
 import no.nav.helse.flex.sykmelding.application.SykmeldingHandterer
+import no.nav.helse.flex.sykmelding.domain.HendelseStatus
 import no.nav.helse.flex.sykmelding.domain.ISykmeldingRepository
 import no.nav.helse.flex.utils.logger
 import no.nav.helse.flex.virksomhet.VirksomhetHenterService
@@ -18,6 +19,9 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 @Controller
 class SykmeldingController(
@@ -59,19 +63,78 @@ class SykmeldingController(
 
     // todo du må i hvert fall filtreere ut nåværende arbeidsgiver
     // todo er det nødvendig å sortere ut arbeidsledig osv? kan vi stole på at de ikke har
+
+    private fun isOverlappende(
+        tidligereSmTom: LocalDate,
+        tidligereSmFom: LocalDate,
+        fom: LocalDate
+    ) =
+        (fom.isAfter(tidligereSmFom) &&
+            fom.isBefore(
+                tidligereSmTom.plusDays(1),
+            ))
+
+    private fun sisteTomIKantMedDag(
+        perioder: List<SykmeldingsperiodeDTO>,
+        dag: LocalDate
+    ): Boolean {
+        val sisteTom =
+            perioder.maxByOrNull { it.tom }?.tom
+                ?: throw IllegalStateException("Skal ikke kunne ha periode uten tom")
+        return !isWorkingdaysBetween(sisteTom, dag)
+    }
+
+    private fun isWorkingdaysBetween(tom: LocalDate, fom: LocalDate): Boolean {
+        val daysBetween = ChronoUnit.DAYS.between(tom, fom).toInt()
+        if (daysBetween < 0) return true
+        return when (fom.dayOfWeek) {
+            DayOfWeek.MONDAY -> daysBetween > 3
+            DayOfWeek.SUNDAY -> daysBetween > 2
+            else -> daysBetween > 1
+        }
+    }
+
+    // finner første fom i gitt sykmelding, en sykmelding kan ha flere perioder
+    private fun findFirstFom(perioder: List<SykmeldingsperiodeDTO>): LocalDate {
+        return perioder.minByOrNull { it.fom }?.fom
+            ?: throw IllegalStateException("Skal ikke kunne ha periode uten fom")
+    }
+
+    // denne henter ikke bare tidlgiere arbeidsgivere, men tidligere arbeidgivere hvor du har vært sykmeldt før
+    // og det er en form for overlapp eller kant i kant som tilsier at du kanskje skal videreføre en sykmelding tidligere arbeidsgivers
+    // til en ny en ??? i hvert fall kunne relevant arbeidsledige
     fun getTidligereArbeidsgivere(
         @PathVariable("sykmeldingId") sykmeldingId: String,
     ): ResponseEntity<List<TidligereArbeidsgiver>> {
         val identer = tokenxValidering.hentIdenter()
-        // val sykmelding = sykmeldingHandterer.hentSykmelding(sykmeldingId, identer)
         val sykmeldinger = sykmeldingHandterer.hentAlleSykmeldinger(identer).filter { it.sykmeldingId != sykmeldingId } // sorterer ut nåværende sykmelding
-        val statuser = sykmeldinger.flatMap {it.statuser}
 
+        // val sykmeldingerKantIKantMedNavaerende = sykmeldinger.filter {}
+        // todo statuslisten burde hete hendelser
+        val statuser = sykmeldinger.flatMap {it.statuser} .filter { it.status == HendelseStatus.SENDT_TIL_ARBEIDSGIVER } // nærmeste status sendt
+        // todo det vi kaller status er mer hendelse? statusene er en liste hendelser, som inneholder status
+
+
+        // tidligere arbeidsgiver brukes kun når bruker har valgt arbeidsledig ... kan ha startet sykmledingen når de var ansatt
+        // og kan så fortsette å være sykmeldt, og da skal det fortsatt være arbeisdtagersøknaded
+        // så da trenger de å finne en sykmelding som er sendt til larbeidsgiver tidligere i sykefraværstilfellet
+        // dette er en sykmelding der det er kant til kant ... en arbeidag imellom teller, om det er helg imellom "teller det ikke"
+        // lørdag og søndag tas med, de tar ikke med helligdager
+        /*
+
+        public final data class SykmeldingHendelse(
+    val databaseId: String? = null,
+    val status: HendelseStatus,
+    val sporsmalSvar: List<Sporsmal>? = null,
+    val arbeidstakerInfo: ArbeidstakerInfo? = null,
+    val opprettet: Instant
+)
+
+         */
         val tidligereArbeidsgivere = statuser
             .mapNotNull { it.arbeidstakerInfo?.arbeidsgiver }
             .map { arbeidsgiver -> TidligereArbeidsgiver(arbeidsgiver.orgnummer, arbeidsgiver.orgnavn) }
-
-
+            .distinct()
 
 
         return ResponseEntity.ok(tidligereArbeidsgivere)
