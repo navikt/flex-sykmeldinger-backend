161d61772e6d8b6e138a30275cfc37055e4797d4
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-03-18 13:06:37 +0100

    logger hele objektet for debug

--- src/main/kotlin/no/nav/helse/flex/listeners/SykmeldingListener.kt
+++ src/main/kotlin/no/nav/helse/flex/listeners/SykmeldingListener.kt
@@ -33,6 +33,7 @@ class SykmeldingListener(
             return
         }
         try {
+            log.info(cr.value())
             val sykmeldingMedBehandlingsutfall: SykmeldingKafkaRecord =
                 objectMapper.readValue(cr.value())
             sykmeldingKafkaLagrer.lagreSykmeldingMedBehandlingsutfall(sykmeldingMedBehandlingsutfall)
