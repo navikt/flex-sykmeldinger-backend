3f65f7bc39b340b2181d9ae6c5c4ec32fdbb32bf
Eirik HS <eirik.skjaerseth@gmail.com>
2025-03-19 16:12:48 +0100

    [wip] utkast BrukerSvar som overtar for Sporsmal og BrukerInfo

--- src/main/kotlin/no/nav/helse/flex/sykmelding/application/ArbeidssituasjonBrukerInfo.kt
+++ src/main/kotlin/no/nav/helse/flex/sykmelding/application/ArbeidssituasjonBrukerInfo.kt
@@ -1,54 +1,140 @@
 package no.nav.helse.flex.sykmelding.application
 
-sealed interface ArbeidssituasjonBrukerInfo {
+import no.nav.helse.flex.api.dto.*
+import no.nav.helse.flex.api.dto.Egenmeldingsperiode
+import java.time.LocalDate
+
+// val erOpplysningeneRiktige: YesOrNo,
+// val arbeidssituasjon: Arbeidssituasjon,
+// val arbeidsgiverOrgnummer: String? = null,
+// val harEgenmeldingsdager: YesOrNo? = null,
+// val riktigNarmesteLeder: YesOrNo? = null,
+// val arbeidsledig: Arbeidsledig? = null,
+// val egenmeldingsdager: List<LocalDate>? = null,
+// val egenmeldingsperioder: List<EgenmeldingsperiodeDTO>? = null,
+// val fisker: Fisker? = null,
+// val harBruktEgenmelding: YesOrNo? = null,
+// val harForsikring: YesOrNo? = null,
+// val uriktigeOpplysninger: List<UriktigeOpplysning>? = null,
+
+sealed interface BrukerSvar {
+    val erOpplysningeneRiktige: Boolean
     val arbeidssituasjon: Arbeidssituasjon
+    val uriktigeOpplysninger: List<UriktigeOpplysning>?
 }
 
-data class ArbeidstakerBrukerInfo(
-    val arbeidsgiverOrgnummer: String? = null,
-) : ArbeidssituasjonBrukerInfo {
+data class ArbeidstakerBrukerSvar(
+    override val erOpplysningeneRiktige: Boolean,
+    val arbeidsgiverOrgnummer: String,
+    val riktigNarmesteLeder: Boolean,
+    val harEgenmeldingsdager: Boolean,
+    val egenmeldingsdager: List<LocalDate>? = null,
+    override val uriktigeOpplysninger: List<UriktigeOpplysning>? = null,
+) : BrukerSvar {
     override val arbeidssituasjon = Arbeidssituasjon.ARBEIDSTAKER
 }
 
-data class ArbeidsledigBrukerInfo(
+data class ArbeidsledigBrukerSvar(
     val arbeidsledigFraOrgnummer: String? = null,
-) : ArbeidssituasjonBrukerInfo {
+) : BrukerSvar {
     override val arbeidssituasjon = Arbeidssituasjon.ARBEIDSLEDIG
 }
 
-data class PermittertBrukerInfo(
+data class PermittertBrukerSvar(
     val arbeidsledigFraOrgnummer: String? = null,
-) : ArbeidssituasjonBrukerInfo {
+) : BrukerSvar {
     override val arbeidssituasjon = Arbeidssituasjon.PERMITTERT
 }
 
-data class FiskerBrukerInfo(
+data class FiskerBrukerSvar(
+    override val erOpplysningeneRiktige: Boolean,
     val lottOgHyre: FiskerLottOgHyre,
+    val blad: FiskerBlad,
     val arbeidsgiverOrgnummer: String? = null,
-) : ArbeidssituasjonBrukerInfo {
+    val riktigNarmesteLeder: Boolean? = null,
+    val harEgenmeldingsdager: Boolean? = null,
+    val egenmeldingsdager: List<LocalDate>? = null,
+    val harBruktEgenmelding: Boolean? = null,
+    val egenmeldingsperioder: List<Egenmeldingsperiode>? = null,
+    val harForsikring: Boolean? = null,
+    override val uriktigeOpplysninger: List<UriktigeOpplysning>? = null,
+) : BrukerSvar {
     override val arbeidssituasjon = Arbeidssituasjon.FISKER
 
+    private val erArbeidstaker = lottOgHyre in setOf(FiskerLottOgHyre.HYRE, FiskerLottOgHyre.BEGGE)
+
+    fun somArbeidstaker(): ArbeidstakerBrukerSvar {
+        require(erArbeidstaker) { "Fisker er ikke arbeidstaker" }
+        return ArbeidstakerBrukerSvar(
+            erOpplysningeneRiktige = erOpplysningeneRiktige,
+            arbeidsgiverOrgnummer = requireNotNull(arbeidsgiverOrgnummer) { "Fisker som er arbeidstaker må ha satt arbeidsgiverOrgnummer" },
+            riktigNarmesteLeder = requireNotNull(riktigNarmesteLeder) { "Fisker som er arbeidstaker må ha satt riktigNarmesteLeder" },
+            harEgenmeldingsdager = requireNotNull(harEgenmeldingsdager) { "Fisker som er arbeidstaker må ha satt harEgenmeldingsdager" },
+            egenmeldingsdager = egenmeldingsdager,
+            uriktigeOpplysninger = uriktigeOpplysninger,
+        )
+    }
+
+    fun somNaringsdrivende(): NaringsdrivendeBrukerSvar {
+        require(!erArbeidstaker) { "Fisker er ikke naringsdrivende" }
+        return NaringsdrivendeBrukerSvar(
+            erOpplysningeneRiktige = erOpplysningeneRiktige,
+            harBruktEgenmelding = requireNotNull(harBruktEgenmelding) { "Fisker som er næringsdrivende må ha satt harBruktEgenmelding" },
+            egenmeldingsperioder = egenmeldingsperioder,
+            harForsikring = requireNotNull(harForsikring) { "Fisker som er næringsdrivende må ha satt harForsikring" },
+            uriktigeOpplysninger = uriktigeOpplysninger,
+        )
+    }
+
     init {
-        if (lottOgHyre in setOf(FiskerLottOgHyre.HYRE, FiskerLottOgHyre.BEGGE)) {
-            require(arbeidsgiverOrgnummer != null) { "ArbeidsgiverOrgnummer må være satt for HYRE" }
+        val erArbeidstaker = runCatching { somArbeidstaker() }.isSuccess
+        val erNaringsdrivende = runCatching { somNaringsdrivende() }.isSuccess
+        if (!erArbeidstaker && !erNaringsdrivende) {
+            throw IllegalArgumentException("Fisker må ha satt felter tilknyttet arbeidsgiver eller næringsdrivende")
         }
     }
 }
 
-data object FrilanserBrukerInfo : ArbeidssituasjonBrukerInfo {
+data class FrilanserBrukerSvar(
+    override val erOpplysningeneRiktige: Boolean,
+    val harBruktEgenmelding: Boolean,
+    val egenmeldingsperioder: List<Egenmeldingsperiode>? = null,
+    val harForsikring: Boolean,
+    override val uriktigeOpplysninger: List<UriktigeOpplysning>? = null,
+) : BrukerSvar {
     override val arbeidssituasjon = Arbeidssituasjon.FRILANSER
 }
 
-data object JordbrukerBrukerInfo : ArbeidssituasjonBrukerInfo {
+data class JordbrukerBrukerSvar(
+    override val erOpplysningeneRiktige: Boolean,
+    val harBruktEgenmelding: Boolean,
+    val egenmeldingsperioder: List<Egenmeldingsperiode>? = null,
+    val harForsikring: Boolean,
+    override val uriktigeOpplysninger: List<UriktigeOpplysning>? = null,
+) : BrukerSvar {
     override val arbeidssituasjon = Arbeidssituasjon.JORDBRUKER
 }
 
-data object NaringsdrivendeBrukerInfo : ArbeidssituasjonBrukerInfo {
+data class NaringsdrivendeBrukerSvar(
+    override val erOpplysningeneRiktige: Boolean,
+    val harBruktEgenmelding: Boolean,
+    val egenmeldingsperioder: List<Egenmeldingsperiode>? = null,
+    val harForsikring: Boolean,
+    override val uriktigeOpplysninger: List<UriktigeOpplysning>? = null,
+) : BrukerSvar {
     override val arbeidssituasjon = Arbeidssituasjon.NAERINGSDRIVENDE
 }
 
-data object AnnetArbeidssituasjonBrukerInfo : ArbeidssituasjonBrukerInfo {
+data class AnnetArbeidssituasjonBrukerSvar(
+    override val erOpplysningeneRiktige: Boolean,
+    override val uriktigeOpplysninger: List<UriktigeOpplysning>? = null,
+) : BrukerSvar {
     override val arbeidssituasjon = Arbeidssituasjon.ANNET
+}
+
+enum class FiskerBlad {
+    A,
+    B,
 }
 
 enum class FiskerLottOgHyre {
@@ -56,3 +142,8 @@ enum class FiskerLottOgHyre {
     HYRE,
     BEGGE,
 }
+
+data class Egenmeldingsperiode(
+    val fom: LocalDate?,
+    val tom: LocalDate?,
+)
