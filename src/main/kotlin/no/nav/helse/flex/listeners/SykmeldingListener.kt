4c5ed6e3cb692a6f12be9dde26ef49878739d282
Eirik HS <eirik.skjaerseth@gmail.com>
2025-04-08 15:14:47 +0200

    fjern ubrukt dependency

--- src/main/kotlin/no/nav/helse/flex/listeners/SykmeldingListener.kt
+++ src/main/kotlin/no/nav/helse/flex/listeners/SykmeldingListener.kt
@@ -2,7 +2,6 @@ package no.nav.helse.flex.listeners
 
 import com.fasterxml.jackson.core.JacksonException
 import com.fasterxml.jackson.module.kotlin.readValue
-import no.nav.helse.flex.config.EnvironmentToggles
 import no.nav.helse.flex.sykmelding.application.SykmeldingKafkaLagrer
 import no.nav.helse.flex.sykmelding.domain.SykmeldingKafkaRecord
 import no.nav.helse.flex.utils.logger
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Component
 @Component
 class SykmeldingListener(
     private val sykmeldingKafkaLagrer: SykmeldingKafkaLagrer,
-    private val environmentToggles: EnvironmentToggles,
 ) {
     val log = logger()
 
