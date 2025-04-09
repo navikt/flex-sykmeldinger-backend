882f93a39acbc1ad9785b83c874ba28a4f194f57
Eirik HS <eirik.skjaerseth@gmail.com>
2025-04-09 16:15:16 +0200

    legg til secure log logging ved oppstart

--- src/main/kotlin/no/nav/helse/flex/Application.kt
+++ src/main/kotlin/no/nav/helse/flex/Application.kt
@@ -1,9 +1,14 @@
 package no.nav.helse.flex
 
+import no.nav.helse.flex.utils.LogMarker
+import no.nav.helse.flex.utils.logger
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 @EnableCaching
@@ -12,4 +17,15 @@ class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
+}
+
+// For debugging purposes
+@Component
+class AfterInit : ApplicationRunner {
+    val logger = logger()
+
+    override fun run(args: ApplicationArguments) {
+        logger.info("Logger til secure logs")
+        logger.info(LogMarker.SECURE_LOGS, "Secure Log melding")
+    }
 }
