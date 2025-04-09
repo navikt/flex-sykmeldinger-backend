e7d40894f755964eca9d1c0213bbbe9e0e7d8ba0
Eirik HS <eirik.skjaerseth@gmail.com>
2025-04-09 16:01:14 +0200

    legg til secure log appender. Bruker SECURE_LOG marker

--- src/main/kotlin/no/nav/helse/flex/utils/Log.kt
+++ src/main/kotlin/no/nav/helse/flex/utils/Log.kt
@@ -2,5 +2,11 @@ package no.nav.helse.flex.utils
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.slf4j.Marker
+import org.slf4j.MarkerFactory
 
 inline fun <reified T> T.logger(): Logger = LoggerFactory.getLogger(T::class.java)
+
+object LogMarker {
+    val SECURE_LOGS: Marker = MarkerFactory.getMarker("SECURE_LOG")
+}
