78ea68303c36eb205a6a3f0d4b9d6466f5924e2e
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-05-30 11:20:05 +0200

    fikser dummy test

--- src/test/kotlin/no/nav/helse/flex/api/SykmeldingControllerTest.kt
+++ src/test/kotlin/no/nav/helse/flex/api/SykmeldingControllerTest.kt
@@ -1,9 +1,7 @@
 package no.nav.helse.flex.api
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class SykmeldingControllerTest {
     @Test
     fun `dummy test`() {
