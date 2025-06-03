bf58297f0657c5a5751587bedce0c90fbc8cd9e8
Eirik HS <eirik.skjaerseth@gmail.com>
2025-06-03 12:54:40 +0200

    fjern kafka batch

--- src/main/kotlin/no/nav/helse/flex/listeners/TestRunSykmeldingListener.kt
+++ src/main/kotlin/no/nav/helse/flex/listeners/TestRunSykmeldingListener.kt
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.flex.sykmelding.domain.SykmeldingKafkaRecord
 import no.nav.helse.flex.utils.logger
 import no.nav.helse.flex.utils.objectMapper
-import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.consumer.ConsumerRecords
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
@@ -16,25 +15,19 @@ class TestRunSykmeldingListener {
 
     @KafkaListener(
         topics = [SYKMELDING_TOPIC],
-        containerFactory = "aivenKafkaBatchListenerContainerFactory",
-        properties = [
-            "auto.offset.reset = earliest",
-            "${ConsumerConfig.MAX_POLL_RECORDS_CONFIG} = 1000",
-        ],
+        containerFactory = "aivenKafkaListenerContainerFactory",
+        properties = ["auto.offset.reset = earliest"],
         id = "flex-sykmeldinger-backend-test-1",
     )
     fun listen(
-        consumerRecords: ConsumerRecords<String, String>,
+        consumerRecord: ConsumerRecord<String, String>,
         acknowledgment: Acknowledgment,
     ) {
-        for (record in consumerRecords) {
-            try {
-                val key: String = record.key()
-                val value: SykmeldingKafkaRecord? =
-                    record.value()?.let { objectMapper.readValue<SykmeldingKafkaRecord>(it) }
-                if (value == null) {
-                    continue
-                }
+        val key: String = consumerRecord.key()
+        try {
+            val value: SykmeldingKafkaRecord? =
+                consumerRecord.value()?.let { objectMapper.readValue<SykmeldingKafkaRecord>(it) }
+            if (value != null) {
                 val sykmeldingId = value.sykmelding.id
 
                 val manglendeSykmeldingId = "280c5631-1217-4bae-8ee8-f875a10718b3"
@@ -48,10 +41,10 @@ class TestRunSykmeldingListener {
                         "SykmeldingId i key og sykmeldingId i value er ikke like. Key: $key, value: $sykmeldingId",
                     )
                 }
-            } catch (e: Exception) {
-                throw RuntimeException("Feil ved behandling av sykmelding på kafka. Melding key: ${record.key()}")
             }
             acknowledgment.acknowledge()
+        } catch (e: Exception) {
+            throw RuntimeException("Feil ved behandling av sykmelding på kafka. Melding key: $key")
         }
     }
 }
