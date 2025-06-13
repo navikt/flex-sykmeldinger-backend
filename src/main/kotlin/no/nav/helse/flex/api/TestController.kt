0b832403677fdb40435ebad7ef052fe196756b4c
Eirik HS <eirik.skjaerseth@gmail.com>
2025-06-13 14:20:31 +0200

    fiks orgnummer param

--- src/main/kotlin/no/nav/helse/flex/api/TestController.kt
+++ src/main/kotlin/no/nav/helse/flex/api/TestController.kt
@@ -18,7 +18,7 @@ class TestController(
     private val log = logger()
 
     @Unprotected
-    @GetMapping("/api/v1/test/kall-aareg/:orgnummer")
+    @GetMapping("/api/v1/test/kall-aareg/{orgnummer}")
     fun kallAareg(
         @PathParam("orgnummer") orgnummer: String? = null,
     ): ResponseEntity<String> {
@@ -29,7 +29,7 @@ class TestController(
             throw IllegalArgumentException("Orgnummer må spesifiseres")
         }
         val res = aaregClient.getArbeidsstedArbeidsforholdoversikt(arbeidsstedOrgnummer = orgnummer)
-        log.info("Test kall til Aareg returnerte for org $orgnummer: \n${res.serialisertTilString()}")
+        log.info("Test kall til Aareg returnerte for org: \n${res.serialisertTilString()}")
 
         return ResponseEntity.ok("Kall til Aareg i test gjennomført, se log")
     }
