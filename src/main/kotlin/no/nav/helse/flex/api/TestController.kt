13c665f5d03b62598bf064eb52c241974e3351dd
Eirik HS <eirik.skjaerseth@gmail.com>
2025-06-13 14:15:58 +0200

    gjør endepunkt ubeskyttet

--- src/main/kotlin/no/nav/helse/flex/api/TestController.kt
+++ src/main/kotlin/no/nav/helse/flex/api/TestController.kt
@@ -1,9 +1,11 @@
 package no.nav.helse.flex.api
 
+import jakarta.websocket.server.PathParam
 import no.nav.helse.flex.clients.aareg.AaregClient
 import no.nav.helse.flex.config.EnvironmentToggles
 import no.nav.helse.flex.utils.logger
 import no.nav.helse.flex.utils.serialisertTilString
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
@@ -15,15 +17,19 @@ class TestController(
 ) {
     private val log = logger()
 
-    @GetMapping("/api/v1/test/kall-aareg")
-    fun kallAareg(): ResponseEntity<String> {
+    @Unprotected
+    @GetMapping("/api/v1/test/kall-aareg/:orgnummer")
+    fun kallAareg(
+        @PathParam("orgnummer") orgnummer: String? = null,
+    ): ResponseEntity<String> {
         if (environmentToggles.isProduction()) {
             throw RuntimeException("Kan ikke kalle test endepunkt i produksjon")
         }
-
-        val testOrg = "214613182"
-        val res = aaregClient.getArbeidsstedArbeidsforholdoversikt(arbeidsstedOrgnummer = testOrg)
-        log.info("Test kall til Aareg returnerte for org $testOrg: \n${res.serialisertTilString()}")
+        if (orgnummer == null) {
+            throw IllegalArgumentException("Orgnummer må spesifiseres")
+        }
+        val res = aaregClient.getArbeidsstedArbeidsforholdoversikt(arbeidsstedOrgnummer = orgnummer)
+        log.info("Test kall til Aareg returnerte for org $orgnummer: \n${res.serialisertTilString()}")
 
         return ResponseEntity.ok("Kall til Aareg i test gjennomført, se log")
     }
