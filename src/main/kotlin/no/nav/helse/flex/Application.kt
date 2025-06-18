0782069419da7c6facdd15ff1311dec6b0c210ff
Eirik HS <eirik.skjaerseth@gmail.com>
2025-06-18 12:59:59 +0200

    enable scheduling only in deployed environments

--- src/main/kotlin/no/nav/helse/flex/Application.kt
+++ src/main/kotlin/no/nav/helse/flex/Application.kt
@@ -4,11 +4,19 @@ import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableCaching
 @EnableJwtTokenValidation
 class Application
+
+@Profile("default")
+@Configuration
+@EnableScheduling
+class DeployApplicationConfig
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
