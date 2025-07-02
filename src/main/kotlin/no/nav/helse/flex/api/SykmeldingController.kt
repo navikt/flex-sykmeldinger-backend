516d8f6ff3c483cd3393ebb76323e90d8e488103
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-07-02 13:17:20 +0200

    limited log

--- src/main/kotlin/no/nav/helse/flex/api/SykmeldingController.kt
+++ src/main/kotlin/no/nav/helse/flex/api/SykmeldingController.kt
@@ -110,7 +110,7 @@ class SykmeldingController(
 
         val sykmeldingPeriode = sykmelding.fom to sykmelding.tom
         val arbeidsgiverDetaljer = arbeidsgiverDetaljerService.hentArbeidsgiverDetaljerForPerson(identer, sykmeldingPeriode)
-        logger.info("Arbeidsgiverdetaljer")
+        logger.info("Arbeidsgiverdetaljer: $arbeidsgiverDetaljer")
 
         return ResponseEntity.ok(
             BrukerinformasjonDTO(
