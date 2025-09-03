b2acf46bc3befd5dde5aac7e352c587003644c4b
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-09-03 16:48:14 +0200

    Lager table for import av all APEN status fra tsm

---
+++ src/main/resources/db/migration/V34__temp_sykmeldinghendelser_apen_fra_tsm.sql
@@ -0,0 +1,20 @@
+CREATE TABLE
+    "public".temp_sykmeldinghendelser_apen_fra_tsm
+(
+    id                     varchar(36) DEFAULT uuid_generate_v4() NOT NULL
+        CONSTRAINT
+            sykmeldingstatus_pkey
+            PRIMARY KEY,
+    sykmelding_id          text                                   NOT NULL,
+    status                 text                                   NOT NULL,
+    tidligere_arbeidsgiver jsonb,
+    bruker_svar            jsonb,
+    tilleggsinfo           jsonb,
+    hendelse_opprettet     timestamp
+                               WITH
+                               time zone                          NOT NULL,
+    lokalt_opprettet       timestamp
+                               WITH
+                               time zone                          NOT NULL,
+    SOURCE                 varchar(256)
+);
