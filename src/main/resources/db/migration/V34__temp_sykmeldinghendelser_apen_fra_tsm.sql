656de0afb81058873746fc5853d569af3f4d400e
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-09-03 16:52:56 +0200

    fikser bugs

--- src/main/resources/db/migration/V34__temp_sykmeldinghendelser_apen_fra_tsm.sql
+++ src/main/resources/db/migration/V34__temp_sykmeldinghendelser_apen_fra_tsm.sql
@@ -1,20 +1,13 @@
 CREATE TABLE IF NOT EXISTS
     "public".temp_sykmeldinghendelser_apen_fra_tsm
 (
-    id                     varchar(36) DEFAULT uuid_generate_v4() NOT NULL
-        CONSTRAINT
-            sykmeldingstatus_pkey
-            PRIMARY KEY,
+    id                     varchar(36) DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
     sykmelding_id          text                                   NOT NULL,
     status                 text                                   NOT NULL,
     tidligere_arbeidsgiver jsonb,
     bruker_svar            jsonb,
     tilleggsinfo           jsonb,
-    hendelse_opprettet     timestamp
-                               WITH
-                               time zone                          NOT NULL,
-    lokalt_opprettet       timestamp
-                               WITH
-                               time zone                          NOT NULL,
+    hendelse_opprettet     timestamp WITH time zone               NOT NULL,
+    lokalt_opprettet       timestamp WITH time zone               NOT NULL,
     SOURCE                 varchar(256)
 );
