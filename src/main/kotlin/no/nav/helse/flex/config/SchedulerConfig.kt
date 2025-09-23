dcffc7dcc891a497875536c97ee4656669bf62f1
Eirik HS <eirik.skjaerseth@gmail.com>
2025-09-23 12:47:38 +0200

    add virtualThreadExecutor

--- src/main/kotlin/no/nav/helse/flex/config/SchedulerConfig.kt
+++ src/main/kotlin/no/nav/helse/flex/config/SchedulerConfig.kt
@@ -2,16 +2,29 @@ package no.nav.helse.flex.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.core.task.TaskExecutor
+import org.springframework.core.task.support.TaskExecutorAdapter
 import org.springframework.scheduling.TaskScheduler
+import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
+import java.util.concurrent.Executors
 
 @Configuration
+@EnableAsync
 class SchedulerConfig {
     @Bean
+    @Primary
     fun taskScheduler(): TaskScheduler =
         ThreadPoolTaskScheduler().apply {
             poolSize = 5
             setThreadNamePrefix("flex-sykmeldinger-backend-scheduled-task-")
             initialize()
         }
+
+    @Bean
+    fun virtualThreadExecutor(): TaskExecutor =
+        TaskExecutorAdapter(
+            Executors.newVirtualThreadPerTaskExecutor(),
+        )
 }
