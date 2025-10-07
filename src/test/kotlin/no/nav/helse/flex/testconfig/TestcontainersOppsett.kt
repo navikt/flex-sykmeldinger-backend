08b1fb7bc4281a1ceaeba813eaee3f613f05f7fb
eirikhs <eirik.skjaerseth@gmail.com>
2025-10-07 16:47:04 +0200

    Application test prod (#374)
    * trekk ut testcontainer oppsett fra integrasjonTestOppsett
    
    * Gjør Testcontainers oppsett til ikke abstrakt
    
    * refaktorer MockWebServereConfig
    
    * la ApplicationTest bruke default profile og ingen overskrevne beans
    
    * fjern DynamicPropertySources, bruk System.setProperty

---
+++ src/test/kotlin/no/nav/helse/flex/testconfig/TestcontainersOppsett.kt
@@ -0,0 +1,71 @@
+package no.nav.helse.flex.testconfig
+
+import no.nav.helse.flex.utils.logger
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.kafka.KafkaContainer
+import org.testcontainers.utility.DockerImageName
+
+// TODO: Bytt fra latest til en konkret versjon når det er tilgjengelig i dockerhub
+private class ValkeyContainer : GenericContainer<ValkeyContainer>("bitnamisecure/valkey:latest")
+
+private class PostgreSQLContainer14 : PostgreSQLContainer<PostgreSQLContainer14>("postgres:14-alpine")
+
+object TestcontainersOppsett {
+    private val logger = logger()
+
+    private val kafkaContainer =
+        KafkaContainer(DockerImageName.parse("apache/kafka-native:3.8.1")).apply {
+            start()
+            System.setProperty("KAFKA_BROKERS", bootstrapServers)
+
+            logger.info(
+                "Started kafka testcontainer: bootstrapServers=${this.bootstrapServers}, " +
+                    "image=${this.dockerImageName}, containerId=${this.containerId.take(12)}",
+            )
+        }
+
+    private val postgresContainer =
+        PostgreSQLContainer14().apply {
+            withCommand("postgres", "-c", "wal_level=logical")
+            start()
+
+            logger.info(
+                "Started Postgres testcontainer: jdbcUrl=${this.jdbcUrl}, " +
+                    "image=${this.dockerImageName}, containerId=${this.containerId.take(12)}",
+            )
+        }
+
+    private val valkeyContainer =
+        ValkeyContainer().apply {
+            withEnv("ALLOW_EMPTY_PASSWORD", "yes")
+            withExposedPorts(6379)
+            start()
+
+            logger.info(
+                "Started Velkey testcontainer: host=${this.host}, port=${this.firstMappedPort}, " +
+                    "image=${this.dockerImageName}, containerId=${this.containerId.take(12)}",
+            )
+        }
+
+    init {
+        postgresContainer.run {
+            System.setProperty("spring.datasource.url", "$jdbcUrl&reWriteBatchedInserts=true")
+            System.setProperty("spring.datasource.username", username)
+            System.setProperty("spring.datasource.password", password)
+        }
+        kafkaContainer.run {
+            System.setProperty("KAFKA_BROKERS", bootstrapServers)
+        }
+        valkeyContainer.run {
+            System.setProperty("VALKEY_HOST_SESSIONS", host)
+            System.setProperty("VALKEY_PORT_SESSIONS", firstMappedPort.toString())
+            System.setProperty("VALKEY_USERNAME_SESSIONS", "default")
+            System.setProperty("VALKEY_PASSWORD_SESSIONS", "")
+        }
+    }
+
+    fun initIfNotRunning() {
+        // trigger init
+    }
+}
