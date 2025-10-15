504a76cbb29bca1bd450e9a54f913474fa1d9252
Eirik HS <eirik.skjaerseth@gmail.com>
2025-10-15 11:30:03 +0200

    flytt fisker validering fra SykmeldingHendelseFraKafkaKonverterer til FiskerBrukerSvar

--- src/test/kotlin/no/nav/helse/flex/sykmeldinghendelse/BrukerSvarTest.kt
+++ src/test/kotlin/no/nav/helse/flex/sykmeldinghendelse/BrukerSvarTest.kt
@@ -1,9 +1,14 @@
 package no.nav.helse.flex.sykmeldinghendelse
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.helse.flex.testdata.lagSporsmalSvar
 import no.nav.helse.flex.utils.objectMapper
 import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.shouldBeFalse
+import org.amshove.kluent.shouldBeTrue
+import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
 
 class BrukerSvarTest {
     @Test
@@ -17,4 +22,37 @@ class BrukerSvarTest {
         val deserialisert: SporsmalSvar<String> = objectMapper.readValue(json)
         sporsmalSvar `should be equal to` deserialisert
     }
+
+    @TestFactory
+    fun `burde opprette fisker som naringsdrivende med minimalt besvarte spørsmål`() =
+        listOf(FiskerLottOgHyre.LOTT, FiskerLottOgHyre.BEGGE)
+            .map { lottOgHyre ->
+                DynamicTest.dynamicTest("lottOgHyre: ${lottOgHyre.name}") {
+                    FiskerBrukerSvar(
+                        erOpplysningeneRiktige = lagSporsmalSvar(svar = true),
+                        arbeidssituasjon = lagSporsmalSvar(svar = Arbeidssituasjon.FISKER),
+                        lottOgHyre = lagSporsmalSvar(svar = lottOgHyre),
+                        blad = lagSporsmalSvar(svar = FiskerBlad.B),
+                    ).run {
+                        erSomArbeidstaker.shouldBeFalse()
+                    }
+                }
+            }
+
+    @TestFactory
+    fun `burde opprette fisker som arbeidstaker med minimalt besvarte spørsmål`() =
+        listOf(FiskerLottOgHyre.HYRE, FiskerLottOgHyre.BEGGE)
+            .map { lottOgHyre ->
+                DynamicTest.dynamicTest("lottOgHyre: ${lottOgHyre.name}") {
+                    FiskerBrukerSvar(
+                        erOpplysningeneRiktige = lagSporsmalSvar(svar = true),
+                        arbeidssituasjon = lagSporsmalSvar(svar = Arbeidssituasjon.FISKER),
+                        lottOgHyre = lagSporsmalSvar(svar = lottOgHyre),
+                        blad = lagSporsmalSvar(svar = FiskerBlad.B),
+                        arbeidsgiverOrgnummer = lagSporsmalSvar(svar = "orgnr"),
+                    ).run {
+                        erSomArbeidstaker.shouldBeTrue()
+                    }
+                }
+            }
 }
