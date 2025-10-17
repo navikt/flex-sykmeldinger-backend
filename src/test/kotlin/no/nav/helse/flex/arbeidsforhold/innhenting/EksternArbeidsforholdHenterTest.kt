73d9d955b2f59bbb1e324a6b9debf3ca0a7f4f4a
Eirik HS <eirik.skjaerseth@gmail.com>
2025-10-17 14:18:24 +0200

    refaktorer EksternArbeidsforholdHenterTest, bytt mock med fake

--- src/test/kotlin/no/nav/helse/flex/arbeidsforhold/innhenting/EksternArbeidsforholdHenterTest.kt
+++ src/test/kotlin/no/nav/helse/flex/arbeidsforhold/innhenting/EksternArbeidsforholdHenterTest.kt
@@ -1,226 +1,186 @@
 package no.nav.helse.flex.arbeidsforhold.innhenting
 
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.doThrow
-import com.nhaarman.mockitokotlin2.mock
 import no.nav.helse.flex.arbeidsforhold.ArbeidsforholdType
-import no.nav.helse.flex.config.EnvironmentToggles
 import no.nav.helse.flex.gateways.aareg.*
-import no.nav.helse.flex.gateways.ereg.EregClient
 import no.nav.helse.flex.gateways.ereg.Navn
 import no.nav.helse.flex.gateways.ereg.Nokkelinfo
+import no.nav.helse.flex.testconfig.FakesTestOppsett
+import no.nav.helse.flex.testconfig.fakes.AaregClientFake
+import no.nav.helse.flex.testconfig.fakes.EnvironmentTogglesFake
+import no.nav.helse.flex.testconfig.fakes.EregClientFake
 import org.amshove.kluent.*
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.client.RestClientException
 import java.time.LocalDate
 
-class EksternArbeidsforholdHenterTest {
-    fun eregClientMock(): EregClient =
-        mock {
-            on { hentNokkelinfo(any()) } doReturn Nokkelinfo(Navn("_"))
-        }
+class EksternArbeidsforholdHenterTest : FakesTestOppsett() {
+    @Autowired
+    lateinit var aaregClient: AaregClientFake
 
-    fun aaregClientMock(): AaregClient =
-        mock {
-            on { getArbeidsforholdoversikt(any()) } doReturn ArbeidsforholdoversiktResponse(listOf(lagArbeidsforholdOversikt()))
-        }
+    @Autowired
+    lateinit var eregClient: EregClientFake
 
-    fun environmentTogglesMock(): EnvironmentToggles =
-        mock {
-            on { isProduction() } doReturn true
-        }
+    @Autowired
+    lateinit var environmentToggles: EnvironmentTogglesFake
+
+    @Autowired
+    lateinit var eksternArbeidsforholdHenter: EksternArbeidsforholdHenter
+
+    @AfterEach
+    fun afterEach() {
+        aaregClient.reset()
+        eregClient.reset()
+        environmentToggles.reset()
+    }
 
     @Test
     fun `burde bruke arbeidsforholdInfo fra Aareg`() {
-        val aaregClient: AaregClient =
-            mock {
-                on { getArbeidsforholdoversikt("fnr") } doReturn
-                    ArbeidsforholdoversiktResponse(
-                        listOf(
-                            lagArbeidsforholdOversikt(
-                                navArbeidsforholdId = "navArbeidsforholdId",
-                                typeKode = "ordinaertArbeidsforhold",
-                                arbeidstakerIdenter =
-                                    listOf(
-                                        Ident(
-                                            type = IdentType.FOLKEREGISTERIDENT,
-                                            ident = "00000000001",
-                                            gjeldende = true,
-                                        ),
-                                    ),
-                                arbeidsstedIdent =
+        aaregClient.setArbeidsforholdoversikt(
+            fnr = "fnr",
+            arbeidsforhold =
+                ArbeidsforholdoversiktResponse(
+                    listOf(
+                        lagArbeidsforholdOversikt(
+                            navArbeidsforholdId = "navArbeidsforholdId",
+                            typeKode = "ordinaertArbeidsforhold",
+                            arbeidstakerIdenter =
+                                listOf(
                                     Ident(
-                                        type = IdentType.ORGANISASJONSNUMMER,
-                                        ident = "orgnummer",
+                                        type = IdentType.FOLKEREGISTERIDENT,
+                                        ident = "00000000001",
+                                        gjeldende = true,
                                     ),
-                                opplysningspliktig =
-                                    Ident(
-                                        type = IdentType.ORGANISASJONSNUMMER,
-                                        ident = "juridisk-orgnummer",
-                                    ),
-                                startdato = LocalDate.parse("2020-01-01"),
-                                sluttdato = LocalDate.parse("2020-01-02"),
-                            ),
+                                ),
+                            arbeidsstedIdent =
+                                Ident(
+                                    type = IdentType.ORGANISASJONSNUMMER,
+                                    ident = "orgnummer",
+                                ),
+                            opplysningspliktig =
+                                Ident(
+                                    type = IdentType.ORGANISASJONSNUMMER,
+                                    ident = "juridisk-orgnummer",
+                                ),
+                            startdato = LocalDate.parse("2020-01-01"),
+                            sluttdato = LocalDate.parse("2020-01-02"),
                         ),
-                    )
-            }
-        val eksternArbeidsforholdHenter =
-            EksternArbeidsforholdHenter(
-                aaregClient = aaregClient,
-                eregClient = eregClientMock(),
-                environmentToggles = environmentTogglesMock(),
-            )
-        val eksterntArbeidsforhold = eksternArbeidsforholdHenter.hentEksterneArbeidsforholdForPerson("fnr").eksterneArbeidsforhold.first()
-        eksterntArbeidsforhold.navArbeidsforholdId `should be equal to` "navArbeidsforholdId"
-        eksterntArbeidsforhold.arbeidsforholdType `should be equal to` ArbeidsforholdType.ORDINAERT_ARBEIDSFORHOLD
-        eksterntArbeidsforhold.orgnummer `should be equal to` "orgnummer"
-        eksterntArbeidsforhold.juridiskOrgnummer `should be equal to` "juridisk-orgnummer"
-        eksterntArbeidsforhold.fom `should be equal to` LocalDate.parse("2020-01-01")
-        eksterntArbeidsforhold.tom `should be equal to` LocalDate.parse("2020-01-02")
+                    ),
+                ),
+        )
+
+        val resultat = eksternArbeidsforholdHenter.hentEksterneArbeidsforholdForPerson("fnr")
+        resultat.eksterneArbeidsforhold.shouldHaveSingleItem().run {
+            navArbeidsforholdId `should be equal to` "navArbeidsforholdId"
+            arbeidsforholdType `should be equal to` ArbeidsforholdType.ORDINAERT_ARBEIDSFORHOLD
+            orgnummer `should be equal to` "orgnummer"
+            juridiskOrgnummer `should be equal to` "juridisk-orgnummer"
+            fom `should be equal to` LocalDate.parse("2020-01-01")
+            tom `should be equal to` LocalDate.parse("2020-01-02")
+        }
     }
 
     @Test
     fun `burde bruke tom liste når AAREG er nede i dev`() {
-        val aaregClient: AaregClient =
-            mock {
-                on { getArbeidsforholdoversikt("fnr") } doThrow RestClientException("AAREG er nede")
-            }
-        val environmentToggles: EnvironmentToggles =
-            mock {
-                on { isProduction() } doReturn false
-            }
-        val eksternArbeidsforholdHenter =
-            EksternArbeidsforholdHenter(
-                aaregClient = aaregClient,
-                eregClient = eregClientMock(),
-                environmentToggles = environmentToggles,
-            )
-        eksternArbeidsforholdHenter
-            .hentEksterneArbeidsforholdForPerson(
-                "fnr",
-            ).eksterneArbeidsforhold
-            .`should be empty`()
+        aaregClient.setArbeidsforholdoversikt(
+            fnr = "fnr",
+            failure = RestClientException("AAREG er nede"),
+        )
+        environmentToggles.setEnvironment("dev")
+
+        val resultat = eksternArbeidsforholdHenter.hentEksterneArbeidsforholdForPerson("fnr")
+        resultat.eksterneArbeidsforhold.`should be empty`()
     }
 
     @Test
     fun `burde hente org navn fra ereg`() {
-        val eregClient: EregClient =
-            mock {
-                on { hentNokkelinfo(any()) } doReturn Nokkelinfo(Navn("Org Navn"))
-            }
+        aaregClient.setArbeidsforholdoversikt(
+            arbeidsforhold = lagArbeidsforholdOversiktResponse(),
+        )
+        eregClient.setNokkelinfo(
+            nokkelinfo = Nokkelinfo(Navn("Org Navn")),
+        )
 
-        val eksternArbeidsforholdHenter =
-            EksternArbeidsforholdHenter(
-                aaregClient = aaregClientMock(),
-                eregClient = eregClient,
-                environmentToggles = environmentTogglesMock(),
-            )
-        val eksterntArbeidsforhold = eksternArbeidsforholdHenter.hentEksterneArbeidsforholdForPerson("_").eksterneArbeidsforhold.first()
-        eksterntArbeidsforhold.orgnavn `should be equal to` "Org Navn"
+        val resultat = eksternArbeidsforholdHenter.hentEksterneArbeidsforholdForPerson("_")
+        resultat.eksterneArbeidsforhold.shouldHaveSingleItem().run {
+            orgnavn `should be equal to` "Org Navn"
+        }
     }
 
     @Test
     fun `burde returnere riktig orgnummer`() {
-        val aaregClient: AaregClient =
-            mock {
-                on { getArbeidsforholdoversikt(any()) } doReturn
-                    ArbeidsforholdoversiktResponse(
-                        listOf(
-                            lagArbeidsforholdOversikt(
-                                arbeidsstedIdent =
-                                    Ident(
-                                        type = IdentType.ORGANISASJONSNUMMER,
-                                        ident = "orgnummer",
-                                    ),
+        aaregClient.setArbeidsforholdoversikt(
+            ArbeidsforholdoversiktResponse(
+                listOf(
+                    lagArbeidsforholdOversikt(
+                        arbeidsstedIdent =
+                            Ident(
+                                type = IdentType.ORGANISASJONSNUMMER,
+                                ident = "orgnummer",
                             ),
-                        ),
-                    )
-            }
-
-        val eksternArbeidsforholdHenter =
-            EksternArbeidsforholdHenter(
-                aaregClient = aaregClient,
-                eregClient = eregClientMock(),
-                environmentToggles = environmentTogglesMock(),
-            )
-        val eksterntArbeidsforhold = eksternArbeidsforholdHenter.hentEksterneArbeidsforholdForPerson("_").eksterneArbeidsforhold.first()
-        eksterntArbeidsforhold.orgnummer `should be equal to` "orgnummer"
+                    ),
+                ),
+            ),
+        )
+        val result = eksternArbeidsforholdHenter.hentEksterneArbeidsforholdForPerson("_")
+        result.eksterneArbeidsforhold.shouldHaveSingleItem().run {
+            orgnummer `should be equal to` "orgnummer"
+        }
     }
 
     @Test
     fun `burde returnere riktig juridisk orgnummer`() {
-        val aaregClient: AaregClient =
-            mock {
-                on { getArbeidsforholdoversikt(any()) } doReturn
-                    ArbeidsforholdoversiktResponse(
-                        listOf(
-                            lagArbeidsforholdOversikt(
-                                opplysningspliktig =
-                                    Ident(
-                                        type = IdentType.ORGANISASJONSNUMMER,
-                                        ident = "juridisk-orgnummer",
-                                    ),
+        aaregClient.setArbeidsforholdoversikt(
+            ArbeidsforholdoversiktResponse(
+                listOf(
+                    lagArbeidsforholdOversikt(
+                        opplysningspliktig =
+                            Ident(
+                                type = IdentType.ORGANISASJONSNUMMER,
+                                ident = "juridisk-orgnummer",
                             ),
-                        ),
-                    )
-            }
-
-        val eksternArbeidsforholdHenter =
-            EksternArbeidsforholdHenter(
-                aaregClient = aaregClient,
-                eregClient = eregClientMock(),
-                environmentToggles = environmentTogglesMock(),
-            )
-        val eksterntArbeidsforhold = eksternArbeidsforholdHenter.hentEksterneArbeidsforholdForPerson("_").eksterneArbeidsforhold.first()
-        eksterntArbeidsforhold.juridiskOrgnummer `should be equal to` "juridisk-orgnummer"
+                    ),
+                ),
+            ),
+        )
+        val resultat = eksternArbeidsforholdHenter.hentEksterneArbeidsforholdForPerson("_")
+        resultat.eksterneArbeidsforhold.shouldHaveSingleItem().run {
+            juridiskOrgnummer `should be equal to` "juridisk-orgnummer"
+        }
     }
 
     @Test
     fun `burde returnere riktig arbeidsforholdType`() {
-        val aaregClient: AaregClient =
-            mock {
-                on { getArbeidsforholdoversikt(any()) } doReturn
-                    ArbeidsforholdoversiktResponse(
-                        listOf(
-                            lagArbeidsforholdOversikt(
-                                typeKode = "frilanserOppdragstakerHonorarPersonerMm",
-                            ),
-                        ),
-                    )
-            }
-
-        val eksternArbeidsforholdHenter =
-            EksternArbeidsforholdHenter(
-                aaregClient = aaregClient,
-                eregClient = eregClientMock(),
-                environmentToggles = environmentTogglesMock(),
-            )
-        val eksterntArbeidsforhold = eksternArbeidsforholdHenter.hentEksterneArbeidsforholdForPerson("_").eksterneArbeidsforhold.first()
-        eksterntArbeidsforhold.arbeidsforholdType `should be equal to` ArbeidsforholdType.FRILANSER_OPPDRAGSTAKER_HONORAR_PERSONER_MM
+        aaregClient.setArbeidsforholdoversikt(
+            ArbeidsforholdoversiktResponse(
+                listOf(
+                    lagArbeidsforholdOversikt(
+                        typeKode = "frilanserOppdragstakerHonorarPersonerMm",
+                    ),
+                ),
+            ),
+        )
+        val result = eksternArbeidsforholdHenter.hentEksterneArbeidsforholdForPerson("_")
+        result.eksterneArbeidsforhold.shouldHaveSingleItem().run {
+            arbeidsforholdType `should be equal to` ArbeidsforholdType.FRILANSER_OPPDRAGSTAKER_HONORAR_PERSONER_MM
+        }
     }
 
     @Test
     fun `burde filtrere vekk arbeidsstedType som ikke er Underenhet`() {
-        val aaregClient: AaregClient =
-            mock {
-                on { getArbeidsforholdoversikt(any()) } doReturn
-                    ArbeidsforholdoversiktResponse(
-                        listOf(
-                            lagArbeidsforholdOversikt(
-                                arbeidsstedType = ArbeidsstedType.Person,
-                            ),
-                        ),
-                    )
-            }
+        aaregClient.setArbeidsforholdoversikt(
+            ArbeidsforholdoversiktResponse(
+                listOf(
+                    lagArbeidsforholdOversikt(
+                        arbeidsstedType = ArbeidsstedType.Person,
+                    ),
+                ),
+            ),
+        )
 
-        val eksternArbeidsforholdHenter =
-            EksternArbeidsforholdHenter(
-                aaregClient = aaregClient,
-                eregClient = eregClientMock(),
-                environmentToggles = environmentTogglesMock(),
-            )
-        eksternArbeidsforholdHenter.hentEksterneArbeidsforholdForPerson("_").eksterneArbeidsforhold shouldHaveSize 0
+        val resultat = eksternArbeidsforholdHenter.hentEksterneArbeidsforholdForPerson("_")
+        resultat.eksterneArbeidsforhold shouldHaveSize 0
     }
 
     private fun lagArbeidsforholdOversikt(
