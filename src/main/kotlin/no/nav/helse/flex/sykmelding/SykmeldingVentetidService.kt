03117f1b9dcb0aee0e7e961ee9bb3971e1e679ca
Steinar Sørli Hegna <steinar.sorli.hegna@nav.no>
2026-04-30 17:38:09 +0200

    Legg til sjekk av arbeidssituasjoner med tilsvarende ventetid, og kun 16 dager før sykmeldingen.

--- src/main/kotlin/no/nav/helse/flex/sykmelding/SykmeldingVentetidService.kt
+++ src/main/kotlin/no/nav/helse/flex/sykmelding/SykmeldingVentetidService.kt
@@ -2,8 +2,17 @@ package no.nav.helse.flex.sykmelding
 
 import no.nav.helse.flex.gateways.syketilfelle.SyketilfelleClient
 import no.nav.helse.flex.sykmeldinghendelse.Arbeidssituasjon
+import no.nav.helse.flex.sykmeldinghendelse.Arbeidssituasjon.*
+import no.nav.helse.flex.sykmeldinghendelse.FiskerBlad
+import no.nav.helse.flex.sykmeldinghendelse.FiskerBrukerSvar
+import no.nav.helse.flex.sykmeldinghendelse.FiskerLottOgHyre
+import no.nav.helse.flex.sykmeldinghendelse.FrilanserBrukerSvar
+import no.nav.helse.flex.sykmeldinghendelse.JordbrukerBrukerSvar
+import no.nav.helse.flex.sykmeldinghendelse.NaringsdrivendeBrukerSvar
 import no.nav.helse.flex.utils.logger
 import org.springframework.stereotype.Service
+
+const val VENTETID_ANTALL_DAGER = 16L
 
 @Service
 class SykmeldingVentetidService(
@@ -24,19 +33,35 @@ class SykmeldingVentetidService(
 
         val tidligsteSykmelding =
             sykmeldingLeser
-                .hentAlleSykmeldingerFraIderFom(sykmeldingerMedSammeVentetid, null)
-                .filter {
-                    it
-                        .sisteHendelse()
-                        .brukerSvar
-                        ?.arbeidssituasjon
-                        ?.svar == arbeidssituasjon
-                }.minByOrNull { it.fom }
+                .hentAlleSykmeldingerFraIderFom(sykmeldingerMedSammeVentetid, sykmelding.fom.minusDays(VENTETID_ANTALL_DAGER))
+                .filter { it.tilsvarendeVentetidForArbeidssituasjon(arbeidssituasjon) }
+                .minByOrNull { it.fom }
 
         val erForsteSykmelding = if (tidligsteSykmelding != null) sykmelding.fom <= tidligsteSykmelding.fom else true
         logger.info(
             "Sykmelding ${sykmelding.sykmeldingId} arbeidssituasjon $arbeidssituasjon er forst: $erForsteSykmelding tidligste eksisterende ${tidligsteSykmelding?.fom}",
         )
         return erForsteSykmelding
+    }
+
+    companion object {
+        fun Sykmelding.tilsvarendeVentetidForArbeidssituasjon(arbeidssituasjon: Arbeidssituasjon): Boolean =
+            when (arbeidssituasjon) {
+                FISKER,
+                NAERINGSDRIVENDE,
+                ->
+                    when (val brukerSvar = this.sisteHendelse().brukerSvar) {
+                        is NaringsdrivendeBrukerSvar -> true
+                        is FiskerBrukerSvar -> brukerSvar.blad.svar == FiskerBlad.A && brukerSvar.lottOgHyre.svar == FiskerLottOgHyre.LOTT
+                        else -> false
+                    }
+                FRILANSER -> this.sisteHendelse().brukerSvar is FrilanserBrukerSvar
+                JORDBRUKER -> this.sisteHendelse().brukerSvar is JordbrukerBrukerSvar
+                ARBEIDSTAKER,
+                ARBEIDSLEDIG,
+                PERMITTERT,
+                ANNET,
+                -> throw IllegalArgumentException("Ventetid er ikke relevant for Arbeidssituasjon $arbeidssituasjon")
+            }
     }
 }
