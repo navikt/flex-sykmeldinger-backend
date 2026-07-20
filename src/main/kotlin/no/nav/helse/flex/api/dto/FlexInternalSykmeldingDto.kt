a54f70257ce69f3d1518dc210270f7ea03c0d7e0
Ole Bastian Løchen <42940656+olebaba@users.noreply.github.com>
2026-07-20 13:08:46 +0200

    Returnerer hendelser for sykmeldinger så vi kan se det i flex internal frontend (#600)

--- src/main/kotlin/no/nav/helse/flex/api/dto/FlexInternalSykmeldingDto.kt
+++ src/main/kotlin/no/nav/helse/flex/api/dto/FlexInternalSykmeldingDto.kt
@@ -1,5 +1,9 @@
 package no.nav.helse.flex.api.dto
 
+import com.fasterxml.jackson.databind.JsonNode
+import no.nav.helse.flex.sykmeldinghendelse.SykmeldingHendelse
+import no.nav.helse.flex.utils.toJsonNode
+import java.time.Instant
 import java.time.LocalDate
 import java.time.OffsetDateTime
 
@@ -7,6 +11,27 @@ data class FlexInternalPasientDto(
     val fnr: String? = null,
     val overSyttiAar: Boolean? = null,
 )
+
+data class FlexInternalHendelseDto(
+    val status: String,
+    val brukerSvar: JsonNode?,
+    val tilleggsinfo: JsonNode?,
+    val source: String?,
+    val hendelseOpprettet: Instant,
+    val lokaltOpprettet: Instant,
+) {
+    companion object {
+        fun fra(hendelse: SykmeldingHendelse) =
+            FlexInternalHendelseDto(
+                status = hendelse.status.name,
+                brukerSvar = hendelse.brukerSvar?.toJsonNode(),
+                tilleggsinfo = hendelse.tilleggsinfo?.toJsonNode(),
+                source = hendelse.source,
+                hendelseOpprettet = hendelse.hendelseOpprettet,
+                lokaltOpprettet = hendelse.lokaltOpprettet,
+            )
+    }
+}
 
 data class FlexInternalSykmeldingDto(
     val id: String,
@@ -16,6 +41,7 @@ data class FlexInternalSykmeldingDto(
     val arbeidsgiver: ArbeidsgiverDTO,
     val sykmeldingsperioder: List<SykmeldingsperiodeDTO>,
     val sykmeldingStatus: SykmeldingStatusDTO,
+    val hendelser: List<FlexInternalHendelseDto>,
     val skjermesForPasient: Boolean,
     val kontaktMedPasient: KontaktMedPasientDTO,
     val behandletTidspunkt: OffsetDateTime,
@@ -27,24 +53,27 @@ data class FlexInternalSykmeldingDto(
     val utenlandskSykmelding: UtenlandskSykmelding?,
 ) {
     companion object {
-        fun fra(sykmeldingDto: SykmeldingDTO) =
-            FlexInternalSykmeldingDto(
-                id = sykmeldingDto.id,
-                pasient = FlexInternalPasientDto(fnr = sykmeldingDto.pasient.fnr, overSyttiAar = sykmeldingDto.pasient.overSyttiAar),
-                mottattTidspunkt = sykmeldingDto.mottattTidspunkt,
-                behandlingsutfall = sykmeldingDto.behandlingsutfall,
-                arbeidsgiver = sykmeldingDto.arbeidsgiver,
-                sykmeldingsperioder = sykmeldingDto.sykmeldingsperioder,
-                sykmeldingStatus = sykmeldingDto.sykmeldingStatus,
-                skjermesForPasient = sykmeldingDto.skjermesForPasient,
-                kontaktMedPasient = sykmeldingDto.kontaktMedPasient,
-                behandletTidspunkt = sykmeldingDto.behandletTidspunkt,
-                syketilfelleStartDato = sykmeldingDto.syketilfelleStartDato,
-                egenmeldt = sykmeldingDto.egenmeldt,
-                papirsykmelding = sykmeldingDto.papirsykmelding,
-                merknader = sykmeldingDto.merknader,
-                signaturDato = sykmeldingDto.signaturDato,
-                utenlandskSykmelding = sykmeldingDto.utenlandskSykmelding,
-            )
+        fun fra(
+            sykmeldingDto: SykmeldingDTO,
+            hendelser: List<SykmeldingHendelse>,
+        ) = FlexInternalSykmeldingDto(
+            id = sykmeldingDto.id,
+            pasient = FlexInternalPasientDto(fnr = sykmeldingDto.pasient.fnr, overSyttiAar = sykmeldingDto.pasient.overSyttiAar),
+            mottattTidspunkt = sykmeldingDto.mottattTidspunkt,
+            behandlingsutfall = sykmeldingDto.behandlingsutfall,
+            arbeidsgiver = sykmeldingDto.arbeidsgiver,
+            sykmeldingsperioder = sykmeldingDto.sykmeldingsperioder,
+            sykmeldingStatus = sykmeldingDto.sykmeldingStatus,
+            hendelser = hendelser.map { FlexInternalHendelseDto.fra(it) },
+            skjermesForPasient = sykmeldingDto.skjermesForPasient,
+            kontaktMedPasient = sykmeldingDto.kontaktMedPasient,
+            behandletTidspunkt = sykmeldingDto.behandletTidspunkt,
+            syketilfelleStartDato = sykmeldingDto.syketilfelleStartDato,
+            egenmeldt = sykmeldingDto.egenmeldt,
+            papirsykmelding = sykmeldingDto.papirsykmelding,
+            merknader = sykmeldingDto.merknader,
+            signaturDato = sykmeldingDto.signaturDato,
+            utenlandskSykmelding = sykmeldingDto.utenlandskSykmelding,
+        )
     }
 }
